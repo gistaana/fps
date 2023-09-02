723ead70868719d7cf27027032b74a5005e72ee0
Gabriel Sta Ana <gistaana8755@gmail.com>
2023-09-02 00:39:05 -0400

    player movement
    finalized player movement for later combat mechanics

---
+++ Player.gd
@@ -0,0 +1,64 @@
+extends CharacterBody3D
+
+var speed
+const WALK = 5.0
+const SPRINT = 8.0
+const JUMP_VELOCITY = 4.5
+const MOUSESPEED = 0.003
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = 9.8
+
+const WALK_FOV = 75.0
+const FOVMULTI = 1.5
+
+@onready var head := $Head # connects to the node which is a child to characterbody3d
+@onready var camera := $Head/Camera3D
+
+func _ready():    # gets rid of cursor to allow camera to move via mouse
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * MOUSESPEED) # moving left to right rotates around the y axis vice versa 
+		camera.rotate_x(-event.relative.y * MOUSESPEED)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60)) # limits rotation of camera
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+		
+	if Input.is_action_pressed("sprint"): # pressing shift will make the player sprint
+		speed = SPRINT
+	else:
+		speed = WALK
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized() # direction moves relative to camera
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0) # limits control in mid air
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 2.0) # limits control in mid air
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 2.0)
+
+	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT * 2)  # moves camera back when sprinting
+	var fov = WALK_FOV + FOVMULTI * velocity_clamped
+	camera.fov = lerp(camera.fov, fov, delta * 8.0)
+	
+	# TESTING
+	
+	move_and_slide()
